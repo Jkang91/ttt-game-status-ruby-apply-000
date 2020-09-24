575692dbcc1162712590acad44d7f307d59b5bb6
June Young Kang <jkang34@gmail.com>
2020-09-24 05:15:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,6 +19,16 @@ def won?(board)
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
+    
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    if position_1 == position_2 && position_2 == positiont_3 && position_taken?(board, index)
+      return win_combination
+    else
+      return false 
+    end
   end
 end
   
