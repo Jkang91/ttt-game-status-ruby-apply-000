d259fb8e5ad5ba2562196c6fac9534e7321a8698
June Young Kang <jkang34@gmail.com>
2020-09-24 04:51:39 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,13 +15,13 @@ WIN_COMBINATIONS = [
   ]
 
 def won?(board)
-  win_index_1 = WIN_COMBINATIONS[0]
-  win_index_2 = win_combinations[1]
-  win_index_3 = win_combinations[2]
-  win_index_4 = win_combinations[3]
-  win_index_5 = win_combinations[4]
-  win_index_6 = win_combinations[5]
-  win_index_7 = win_combinations[6]
-  win_index_8 = win_combinations[7]
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+  win_index_4 = win_combination[3]
+  win_index_5 = win_combination[4]
+  win_index_6 = win_combination[5]
+  win_index_7 = win_combination[6]
+  win_index_8 = win_combination[7]
   
 end
