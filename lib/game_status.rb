d9d5e513f204c28196c4c05edb98f12f35991a1b
June Young Kang <jkang34@gmail.com>
2020-09-24 06:10:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -40,10 +40,14 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     return true 
-  elsif !won?(board) && !full?(board)
-    return false
   else 
     return false
+  end
+end
+
+def over?(board)
+  if won?board && !full?(board)
+    return true
   end
 end
       
